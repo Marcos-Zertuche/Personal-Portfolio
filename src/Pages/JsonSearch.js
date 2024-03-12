2d9b5b3001601383a298276c1d3b40145e90d675
Marcos Zertuche <105753081+Marcos-Zertuche@users.noreply.github.com>
2024-03-12 12:47:24 -0500

    Update to init commit

--- src/Pages/JsonSearch.js
+++ src/Pages/JsonSearch.js
@@ -1,10 +1,10 @@
 import React from "react";
  
-const JSON_Search = () => {
+const jsonSearch = () => {
     return (
         <div>
             <h1>JSON SEARCH ENGINE  here</h1>
         </div>
     );
 };
-export default JSON_Search;
+export default jsonSearch;
