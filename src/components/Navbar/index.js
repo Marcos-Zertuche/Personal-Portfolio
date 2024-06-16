15cf4d42b0b3c5e1eb9f8915e93fa32d784ba16d
Marcos Zertuche <105753081+Marcos-Zertuche@users.noreply.github.com>
2024-06-16 12:48:04 -0500

    Updates

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {
+    AppBar,
+    Box,
+    Toolbar,
+    Typography,
+    IconButton,
+    Switch,
+    MenuItem,
+    Menu
+} from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu';
+
+
+
+export default function Navbar() {
+    <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+            <Toolbar>
+                    
+            </Toolbar>
+        </AppBar>
+    </Box>
+}
